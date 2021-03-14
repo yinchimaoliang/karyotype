6026fa24ee32de289d30a9a6d9eb77a5b4aa2df5
liyinhao <liyinhao0413@qq.com>
2021-03-14 14:38:44 +0800

    Add karyotype_classify.

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,1 +1,2 @@
+from .inference_karyotype_classify import *  # noqa: F401,F403
 from .inference_karyotype_segmentation import *  # noqa: F401,F403
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 
-from apis import inference_segmentor, init_segmentor
+from apis import (inference_detector, inference_segmentor, init_detector,
+                  init_segmentor)
 
 
 def parse_args():
@@ -8,6 +9,13 @@ def parse_args():
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--ckpt-path', help='checkpoint file path')
     parser.add_argument('--img-path', help='image path')
+    parser.add_argument(
+        '--type',
+        choices=[
+            'karyotype_segment', 'karyotype_classify', 'karyotype_count',
+            'karyotype_polarity'
+        ],
+        help='type of the job')
     args = parser.parse_args()
 
     return args
@@ -19,12 +27,21 @@ def karyotype_segment(config_path, ckpt_path, img_path):
     print(result)
 
 
+def karyotype_classify(config_path, ckpt_path, img_path):
+    detector = init_detector(config_path, ckpt_path)
+    result = inference_detector(detector, img_path)
+    print(result)
+
+
 def main():
     args = parse_args()
     config_path = args.config
     ckpt_path = args.ckpt_path
     img_path = args.img_path
-    karyotype_segment(config_path, ckpt_path, img_path)
+    if args.type == 'karyotype_segment':
+        karyotype_segment(config_path, ckpt_path, img_path)
+    if args.type == 'karyotype_classify':
+        karyotype_classify(config_path, ckpt_path, img_path)
 
 
 if __name__ == '__main__':
